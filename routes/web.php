62779e8fb08b3851606bdcc372405b38546c19a4
AMINE <amineelhailaa@gmail.com>
2026-01-29 19:14:21 +0100

    Improve web (routes)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('home');
@@ -17,3 +18,22 @@ require __DIR__.'/auth.php';
 
 
 Route::get('/dashboard',[UserController::class,'index'])->name('dashboard');
+
+
+//to show profiles after 
+Route::get('/profile/{id}',function( $id){
+$user = User::find($id);
+echo $user;
+});
+
+//edit profile my profile
+Route::middleware('auth')->group(function(){
+
+
+Route::get('/myprofile',[ProfileController::class,'myprofile'])->name('myprofile');
+Route::patch('/myprofile',[ProfileController::class,'update']);
+
+
+});
+
+
