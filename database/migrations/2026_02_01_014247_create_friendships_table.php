b8245207a669339061f5e5a460f2eb525ad5a234
AMINE <amineelhailaa@gmail.com>
2026-02-01 02:58:41 +0100

    Create friendships table with sender and receiver foreign keys

---
+++ database/migrations/2026_02_01_014247_create_friendships_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('friendships', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sender_id');
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('receiver_id');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('status',['pending','accepted','refused']);
+            $table->unique(['sender_id','receiver_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('friendships');
+    }
+};
