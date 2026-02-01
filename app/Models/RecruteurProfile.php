851be1b16738d4b7ad535b364f5f475b26d0de8b
AMINE <amineelhailaa@gmail.com>
2026-02-01 01:50:06 +0100

    Make recruiter profile attributes mass assignable

--- app/Models/RecruteurProfile.php
+++ app/Models/RecruteurProfile.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RecruteurProfile extends Model
 {
     //
     protected $fillable = ['entreprise'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
