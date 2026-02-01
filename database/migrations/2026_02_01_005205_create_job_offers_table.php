968a6e1d61722c83d84f9b433e234065ef9bb9f9
AMINE <amineelhailaa@gmail.com>
2026-02-01 02:04:24 +0100

    Create job offers table

---
+++ database/migrations/2026_02_01_005205_create_job_offers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_offers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recruteur_profile_id')->constrained()->cascadeOnDelete();
+            $table->string('entreprise');
+            $table->string('contrat');
+            $table->string('titre');
+            $table->string('description');
+            $table->string('image');
+            $table->enum('status',['open','closed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_offers');
+    }
+};
