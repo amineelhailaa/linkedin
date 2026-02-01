694654ea4be1c2e95179d5a8830ad66a8c9e5e36
AMINE <amineelhailaa@gmail.com>
2026-02-01 02:31:36 +0100

    Expose job offer applications relationship for recruiters

--- app/Models/JobOffer.php
+++ app/Models/JobOffer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class JobOffer extends Model
 {
@@ -19,6 +20,11 @@ class JobOffer extends Model
     public function recruteurProfile(): BelongsTo
     {
         return $this->belongsTo(RecruteurProfile::class);
+    }
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
     }
     //
 }
