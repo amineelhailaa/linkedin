5d1b6632b08e19e2bafc590d76dbeab1a7eddbcf
AMINE <amineelhailaa@gmail.com>
2026-02-01 02:04:18 +0100

    Add recruiter profile relations

--- app/Models/RecruteurProfile.php
+++ app/Models/RecruteurProfile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RecruteurProfile extends Model
 {
@@ -13,6 +14,11 @@ class RecruteurProfile extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function jobOffer(): HasMany
+    {
+        return $this->hasMany(JobOffer::class);
     }
 
 }
