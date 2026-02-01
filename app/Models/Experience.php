aa88a3e3e115452e547910a8e6d1fd6261a6cc5b
AMINE <amineelhailaa@gmail.com>
2026-02-01 01:50:01 +0100

    Link experiences to candidate profile

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Experience extends Model
 {
     protected $fillable = ['entreprise','duree'];
     //
+    public function candidatProfile(): BelongsTo
+    {
+        return $this->belongsTo(CandidatProfile::class);
+    }
 }
