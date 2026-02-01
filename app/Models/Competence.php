5dabf40d2869fd92b89991d23de634ec0ae017e7
AMINE <amineelhailaa@gmail.com>
2026-02-01 01:49:59 +0100

    Link competences to candidate profile

--- app/Models/Competence.php
+++ app/Models/Competence.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Competence extends Model
 {
     protected $fillable = ['titre'];
     //
+    public function candidatProfile(): BelongsTo
+    {
+        return $this->belongsTo(CandidatProfile::class);
+    }
 }
