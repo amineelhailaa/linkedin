a93c3f35769d841f5f546e4cdd7a002661db9f0e
AMINE <amineelhailaa@gmail.com>
2026-02-02 16:40:42 +0100

    working on profile controller logic between recruter and candidat , also the views , data validation

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,6 +11,8 @@ use Illuminate\View\View;
 use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
+use function PHPUnit\Framework\containsOnly;
+use function PHPUnit\Framework\isEmpty;
 
 class ProfileController extends Controller
 {
@@ -24,17 +26,44 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function discoverProfile($id){
+    public function discoverProfile($id)
+    {
         $user = User::find($id);
-        return view('profile.discoverProfile',['user'=>$user]);
+        if ($user) {
+            $user->load(['candidatProfile.competence', 'candidatProfile.formation', 'candidatProfile.experience']);
+        }
+
+        if ($user && $user->type === 'candidat') {
+            return view('profile.candidatProfile', ['user' => $user]);
+        }
+
+        return view('profile.discoverProfile', ['user' => $user]);
     }
 
 
-
-    public function myprofile(){
+    public function myprofile()
+    {
         $user = Auth::user();
-    
-        return view('profile.myprofile',['user'=>$user]);
+
+        return view('profile.myprofile', ['user' => $user]);
+    }
+
+
+   public function validateEmpty($formations)
+    {
+        $fors=[];
+        foreach ($formations as $formation ){
+            $can = true;
+            foreach ($formation as $key=>$value){
+                if (empty($value)){
+                    $can = false;
+                }
+            }
+            if ($can){
+                $fors[]=$formation;
+            }
+        }
+        return $fors;
     }
 
     /**
@@ -42,24 +71,64 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        // a function for validating
 
-        //get data except avatar 
+        //get data except avatar
         $data = $request->safe()->except('pic_path');
-            //if has a file ( can be null means unchanged)
-        if($request->hasFile('pic_path')){
-            $data['pic_path']= $request->file('pic_path')->store('avatars','public');
+        //if has a file ( can be null means unchanged)
+        if ($request->hasFile('pic_path')) {
+            $data['pic_path'] = $request->file('pic_path')->store('avatars', 'public');
         }
 
         //check if the user want to change the password
 
-        if($request->filled('password')){
-            $data['password']= Hash::make($request->password);
+        if ($request->filled('password')) {
+            $data['password'] = Hash::make($request->password);
         } else {
             unset($data['password']);
         }
 
+//        dd('amine');
 
-        Auth::user()->update($data);
+        $user = Auth::user();
+        $user->update($data);
+
+        if ($user->type == 'candidat') {
+            $profile = $user->candidatProfile;
+            $profile->update($data);
+
+            $formations = $request->input('formations', []);
+            $competences = $request->input('competences', []);
+            $experiences = $request->input('experiences', []);
+
+            $formations = $this->validateEmpty($formations);
+            $experiences = $this->validateEmpty($experiences);
+            $competences = $this->validateEmpty($competences);
+
+
+
+
+            $profile->formation()->delete();
+            $profile->competence()->delete();
+            $profile->experience()->delete();
+
+
+            if(!empty($formations)){
+
+                $profile->formation()->createMany($formations);
+
+            }
+            if(!empty($competences)){
+                $profile->competence()->createMany($competences);
+
+            }
+            if(!empty($experiences)){
+                $profile->experience()->createMany($experiences);
+            }
+
+        } elseif ($user->type == 'recruteur') {
+            $user->recruteurProfile->update($data);
+        }
 
         return Redirect::route('myprofile');
     }
