6f9b40a8249dbc41bf571e38f3ab6a053b50cc3c
AMINE <amineelhailaa@gmail.com>
2026-02-04 13:03:08 +0100

    Adjust job offer factory

--- database/factories/JobOfferFactory.php
+++ database/factories/JobOfferFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\JobOffer>
@@ -17,7 +18,7 @@ class JobOfferFactory extends Factory
     public function definition(): array
     {
         return [
-            'recruteur_profile_id' => fake()->int(),
+            'recruteur_profile_id' => fake()->numberBetween(1),
             'entreprise' => fake()->name(),
             'contrat' => fake()->name(),
             'titre' => fake()->name(),
