a7514f07f37cfe25591469d09b03b01d705ced78
AMINE <amineelhailaa@gmail.com>
2026-02-05 16:31:32 +0100

    Update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,17 +12,17 @@ class UserController extends Controller
     public function index(Request $get_request){
         $users = null;
         $type = $get_request->input('type') ?? null;
-        $query = $get_request->input('q') ?? null ; 
+        $query = $get_request->input('q') ?? null ;
         // echo $query;
         if(isset($type)&& isset($query)){
                 // echo "amine elhailaa";
-            $users = User::where("{$type}", 'LIKE', "%{$query}%")->get(); 
-            //does laravel sanitize this , cuz i didnt validate the type so they can pass another column and kick me , 
+            $users = User::where('id', '!=', auth()->id())->where("{$type}", 'LIKE', "%{$query}%")->get();
+            //does laravel sanitize this , cuz i didnt validate the type so they can pass another column and kick me ,
             //iwill add a if else but it will be hard coded in a gro projecgt
             //just making the default is name.
         } else {
                 // code...
-                    $users = User::all();
+            $users = User::where('id', '!=', auth()->id())->get();
             }
 
         return view('dashboard',compact('users'));
