5be62fadea42b5aa7603f56cc574877b342f3020
AMINE <amineelhailaa@gmail.com>
2026-02-06 16:36:04 +0100

    working on integrating the livewire

--- resources/views/components/structure.blade.php
+++ resources/views/components/structure.blade.php
@@ -6,11 +6,12 @@
   <title>{{$title}}</title>
 	@vite(['resources/css/app.css']);
     <script src="https://cdn.tailwindcss.com"></script>
+    @livewireStyles
   </head>
   <body class="min-h-screen bg-white text-slate-900 [font-size:18px] lg:[font-size:19px]">
 
 {{$slot}}
-
+@livewireScripts
 </body>
 </html>
 
